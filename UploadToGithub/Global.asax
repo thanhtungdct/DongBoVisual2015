53b816bfe3ceac264875a8da97d384201535c454
THANHTUNG <THANHTUNG@THANHTUNG>
2017-10-13 10:01:49 +0700

    Add project files.

---
+++ UploadToGithub/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UploadToGithub.MvcApplication" Language="C#" %>
---
+++ UploadToGithub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UploadToGithub.Startup))]
+namespace UploadToGithub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
